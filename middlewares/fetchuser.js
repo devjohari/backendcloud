1a2459904a3b891c9fc9943c4b170c243d98fa2c
Dev Johari <102499062+devjohari@users.noreply.github.com>
2023-01-18 13:33:21 +0530

    Update fetchuser.js

--- middlewares/fetchuser.js
+++ middlewares/fetchuser.js
@@ -11,6 +11,11 @@ const fetchuser = (req, res, next) => {
    try {
       const data = jwt.verify(tokenreq, secret);
       req.user = data.user;
+      res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    });
       next();
    } catch (error) {
       res.status(401).send({
